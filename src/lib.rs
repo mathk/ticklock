dd48c17e595ae4a2675da64acca8d634069b30aa
Mathieu Suen <mathk.sue@gmail.com>
2019-02-26 22:47:24 +0100

    Work on timer.

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! This crate is to provide general purpose abstraction to manipulate time
+//! base quantity in embedded system.
+
+#![no_std]
+
+pub mod timer;
--- src/timer.rs
+++ src/timer.rs
@@ -1,18 +1,16 @@
 //! Provide timer abstraction
 
 // #![allow(missing_docs)]
-use crate::peripheral::SYST;
-use crate::peripheral::syst::SystClkSource;
-use crate::clock::Clocks;
 use core::time::Duration;
-use core::cmp::min;
+use core::ops::Sub;
+use core::convert::Into;
 
 /// A `Timer`` trait to represent count down time.
 /// This is a typical peripheral that has an internal counter that decrease or increase over time until it reach its limit.
 pub trait Timer {
 
     /// Inner type of the counter
-    type U
+    type U : Sub<Output impl Into<u32>>;
 
     /// Pause the execution for Duration.
     fn delay(&mut self, d: Duration);
@@ -27,16 +25,18 @@ pub trait Timer {
     /// Start a timer counter
     /// The timer is being move and dedicated
     /// to the instant needs.
-    fn start(mut self) ->  TimerInstant<Self>;
+    fn start<T: Timer>(self) ->  TimerInstant<T>;
 
     /// Stop the counting timer.
     /// This method is only used by `TimerInstant` to release the timer.
-    fn stop(mut self) -> Self;
+    fn stop(self) -> Self;
 
     /// Test if the counter has wrapped to its initial value
     fn has_wrapped(&mut self) -> bool;
+    fn limit_value(&self) -> Self::U;
     fn get_current(&mut self) -> Self::U;
     fn tick(&mut self) -> Duration;
+}
 
 
 /// Capture an instant from a timer.
@@ -46,11 +46,11 @@ where T : Timer
     delay: T,
 }
 
-impl<T> SysTickInstant<T>
-where T : Clocks
+impl<T> TimerInstant<T>
+where T : Timer
 {
-    fn now(delay: SysTickDelay<T>) -> Self {
-        SysTickInstant {
+    fn now(delay: T) -> Self {
+        TimerInstant {
             delay,
         }
     }
@@ -59,59 +59,10 @@ where T : Clocks
         if self.delay.has_wrapped() {
             panic!("Can not tell the elapse time as we have wrapped.")
         }
-        self.delay.tick() * (0x0FF_FFFF - self.delay.get_current())
+        self.delay.tick() * (self.delay.limit_value() - self.delay.get_current()).into()
     }
 
-    pub fn stop(self) -> SysTickDelay<T> {
+    pub fn stop(self) -> T {
         self.delay.stop()
-    }
-}
-
-/// Delay base on Systick.
-pub struct SysTickDelay<T>
-where T : Clocks
-{
-    syst: SYST,
-    clocks: T,
-}
-
-/// Delay using the SysTick timer
-impl<T> SysTickDelay<T>
-where
-    T: Clocks
-{
-
-    /// Build a new SysTick timer base on external source clock.
-    /// External clock is vendor dependent
-    pub fn new_external(mut syst: SYST, clocks: T) -> Self {
-        syst.set_clock_source(SystClkSource::External);
-        SysTickDelay {
-            syst,
-            clocks
-        }
-    }
-
-    pub fn start(mut self) ->  SysTickInstant<T> {
-        self.syst.set_reload(0x00FF_FFFF);
-        self.syst.clear_current();
-        self.syst.enable_counter();
-        SysTickInstant::now(self)
-    }
-
-    pub fn stop(mut self) -> Self {
-        self.syst.disable_counter();
-        self
-    }
-
-    pub fn tick(&mut self) -> Duration {
-        self.clocks.get_syst_clock(&mut self.syst).tick()
-    }
-
-    pub fn has_wrapped(&mut self) -> bool {
-        self.syst.has_wrapped()
-    }
-
-    pub fn get_current(&mut self) -> u32 {
-        SYST::get_current()
     }
 }
