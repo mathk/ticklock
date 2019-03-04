2687ec1a3caf84f564e2d100ec7a4b589dd7d850
Mathieu Suen <mathk.sue@gmail.com>
2019-03-04 01:42:41 +0100

    Adding tests

--- src/clock.rs
+++ src/clock.rs
@@ -160,3 +160,44 @@ impl U32Ext for u32 {
         Duration::from_micros(self as u64)
     }
 }
+
+
+mod test {
+
+    use super::{FreqRange, U32Ext};
+
+    #[test]
+    fn divide() {
+        assert_eq!((1.mhz() / 2).into_kilo().resolution, FreqRange::KiloHertz);
+        assert_eq!((1.mhz() / 2).into_kilo().numerator, 500);
+        assert_eq!((1.mhz() / 8000).into_hertz().resolution, FreqRange::Hertz);
+        assert_eq!((1.mhz() / 8000).into_hertz().numerator, 125);
+        assert_eq!((1.mhz() / 80000).into_hertz().resolution, FreqRange::Hertz);
+        assert_eq!((1.mhz() / 80000).into_hertz().numerator, 12);
+
+    }
+
+    #[test]
+    fn tick() {
+        assert_eq!(1.mhz().tick(), 1.us());
+        assert_eq!(1.khz().tick(), 1.ms());
+        assert_eq!(1.hz().tick(), 1.s());
+        assert_eq!(1.hz().ticks_in(2.s()), 2);
+    }
+
+    #[test]
+    fn ticks_in() {
+        assert_eq!(1.khz().ticks_in(2.s()), 2_000);
+        assert_eq!(1.mhz().ticks_in(2.s()), 2_000_000);
+        assert_eq!(2.mhz().ticks_in(2.s()), 4_000_000);
+        assert_eq!(2.mhz().ticks_in(2.us()), 4);
+    }
+
+    #[test]
+    fn scale() {
+        assert_eq!(1.mhz() / 500.khz(), 2);
+        assert_eq!(1.mhz() / 1000.khz(), 1);
+        assert_eq!(1.mhz() / 10000.khz(), 0);
+    }
+
+}
